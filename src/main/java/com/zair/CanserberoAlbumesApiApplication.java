f6f4adfacaa88d77780cd1d8155a5227c1b14b5c
ZairBulos <buloszair@gmail.com>
2024-04-29 14:23:11 -0300

    feat: add initial project

---
+++ src/main/java/com/zair/CanserberoAlbumesApiApplication.java
@@ -0,0 +1,13 @@
+package com.zair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanserberoAlbumesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanserberoAlbumesApiApplication.class, args);
+	}
+
+}
